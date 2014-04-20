c1f65e1aa6b961654a91ab26ffcfe39274683d11
Sindre Sorhus <sindresorhus@gmail.com>
2014-04-20 22:33:51 +0200

    init

---
+++ test.js
@@ -0,0 +1,13 @@
+'use strict';
+var assert = require('assert');
+var readChunk = require('read-chunk');
+var isPsd = require('./index');
+
+function check(filename) {
+	return isPsd(readChunk.sync(filename, 0, 4));
+}
+
+it('should detect PSD from Buffer', function () {
+	assert(check('fixture.psd'));
+	assert(!check('fixture.png'));
+});
