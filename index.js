dc810202689025115a8561328e5a8a35d85f2eb7
Sindre Sorhus <sindresorhus@gmail.com>
2021-08-10 12:27:37 +0200

    Require Node.js 12.20 and move to ESM

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-'use strict';
-module.exports = buffer => {
+export default function isPsd(buffer) {
 	if (!buffer || buffer.length < 4) {
 		return false;
 	}
 
-	return buffer[0] === 56 &&
-		buffer[1] === 66 &&
-		buffer[2] === 80 &&
-		buffer[3] === 83;
-};
+	return buffer[0] === 56
+		&& buffer[1] === 66
+		&& buffer[2] === 80
+		&& buffer[3] === 83;
+}
--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 import test from 'ava';
-import readChunk from 'read-chunk';
-import m from '.';
+import {readChunkSync} from 'read-chunk';
+import isPsd from './index.js';
 
-const check = filename => m(readChunk.sync(filename, 0, 4));
+const check = filename => isPsd(readChunkSync(filename, {length: 4}));
 
 test('detects PSD from Buffer', t => {
 	t.true(check('fixture.psd'));
